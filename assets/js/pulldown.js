d6166e1966ee5dde87b73a85b38b1c263a6be0a7
Kenneth Herndon <kenny@kennymac.local>
2025-02-03 06:25:17 -0600

    started work on javascript
    added some id's to the html that the javascript wants in order to work,
    its not functional yet i need to change the way it moves the page
    instead of it being margin related it will be translate based now. also
    i gotta rename those variables now i need to be presentable now!!!

---
+++ assets/js/pulldown.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+            var bruh = document.querySelector('#catch-up');
+            var pageget = document.getElementById('nextpage');
+            var whoIbe = document.getElementById('catch-up');
+            var titleget = document.getElementById('Title');
+    
+            var observermaybe = new IntersectionObserver(function(entries) {
+                if (entries[0].isIntersecting === true){
+                    var titlesize = titleget.offsetHeight - 200;
+                    var whereIbe = window.pageYOffset + whoIbe.getBoundingClientRect().top - 3795 - titlesize;
+                    
+                    console.log(whereIbe)
+                    if(whereIbe > -1204){
+                        pageget.style.marginTop = whereIbe + 'px';
+                        console.log("Get over here!");
+                    }
+                } else {
+                //    pageget.style.marginTop = '-1750px';
+		pageget.style.transform = 'translateY(unset)';
+                }
+            }, { threshold: [0] });
+            observermaybe.observe(bruh);
+        })
