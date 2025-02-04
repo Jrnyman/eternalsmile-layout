54d670a048ac7aea48bc00041721d24857eac1e6
Kenneth Herndon <kengherndon2@gmail.com>
2025-02-03 18:47:11 -0600

    fixed the pulldown script
    it needs to have some type of room at the bottom for #catchup to work
    properly 100% of the time but there isn't any circumstance where
    catuchup would be the last element on the dom- at least on comic fury
    for sure

--- assets/js/pulldown.js
+++ assets/js/pulldown.js
@@ -7,16 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
             var observermaybe = new IntersectionObserver(function(entries) {
                 if (entries[0].isIntersecting === true){
                     var titlesize = titleget.offsetHeight - 200;
-                    var whereIbe = window.pageYOffset + whoIbe.getBoundingClientRect().top - 3795 - titlesize;
+                    var whereIbe = window.pageYOffset + whoIbe.getBoundingClientRect().top - 2175 - titlesize;
                     
                     console.log(whereIbe)
                     if(whereIbe > -1204){
-                        pageget.style.marginTop = whereIbe + 'px';
+                //        pageget.style.marginTop = whereIbe + 'px';
+			pageget.style.transform = 'translateY(' + whereIbe + 'px)';
                         console.log("Get over here!");
                     }
                 } else {
                 //    pageget.style.marginTop = '-1750px';
-		pageget.style.transform = 'translateY(unset)';
+		pageget.style.transform = 'translateY(0)';
                 }
             }, { threshold: [0] });
             observermaybe.observe(bruh);
