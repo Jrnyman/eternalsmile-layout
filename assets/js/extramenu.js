8abee0face4b5195a92b7fc0576ed753ea16fe29
Kenneth Herndon <kengherndon2@gmail.com>
2025-02-16 09:24:33 -0600

    added dropdown menu function
    its crude and i'd like to make it wear it see's that translateY is -100%
    for more than 1 second THEN swaps to display none, but it works for now

--- assets/js/extramenu.js
+++ assets/js/extramenu.js
@@ -1,3 +1,22 @@
-document.addEventListener("keydown", (event) => {
-	if ((event.shiftKey
+var iframe = document.getElementById('tildemenu');
+
+function toggle() {
+	if (iframe.style.display === "none") {
+		iframe.style.display = "block";
+		setTimeout(function(){
+			iframe.style.transform = "translateY(0)";
+		}, 10);
+	} else {
+		iframe.style.transform = "translateY(-100%)";
+		setTimeout(function(){
+			iframe.style.display = "none";
+		}, 450);
+	}
 }
+
+document.addEventListener("keydown", (event) => {
+	if ((event.shiftKey) && event.key === "~") {
+		console.log("naw, bro tryna see");
+		toggle();
+	}
+})
