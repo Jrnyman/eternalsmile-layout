3b33f78dced4308423ce68309ce967071282d940
Kenneth Herndon <kengherndon2@gmail.com>
2025-02-23 09:48:58 -0600

    cleaned up alot of variable declaration
    the theme.js file will hold most of the variables related to all this
    working so far

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,19 @@
 const rootget = document.querySelector(':root');
+const usersays = {
+	primary: document.getElementById("customPrimary"),
+	secondary: document.getElementById("customSecondary"),
+	splash: document.getElementById("customSplash"),
+	ok: document.getElementById("alrighty"),
+	nvm: document.getElementById("reboot"),
+	input: {primary: null, secondary: null, splash: null}
+}
 var local = {
 	ready: localStorage.getItem("theme"),
 	primary: localStorage.getItem("localP"),
 //	secondary: ,
 //	splash: ,
 };
+//const gotcha = ["primary", "secondary", "splash"] 
 function setStyles() {
 	rootget.style.setProperty('--white', local.primary);
 }
