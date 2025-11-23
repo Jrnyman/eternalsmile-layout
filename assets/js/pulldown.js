232ca4ce664c3042b765147136ee9cbc58ea4434
Kenneth Herndon <kengherndon2@gmail.com>
2025-11-23 08:21:33 -0600

    formating adjustments

--- assets/js/pulldown.js
+++ assets/js/pulldown.js
@@ -1,5 +1,6 @@
 var howfardown = [2075, 1575]
 var chosen
+
 document.addEventListener('DOMContentLoaded', function() {
 	var checkpoint = document.querySelector('#catch-up');
 	var pageget = document.getElementById('nextpage');
@@ -24,19 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			break;
 	}
 
-            var observermaybe = new IntersectionObserver(function(entries) {
-                if (entries[0].isIntersecting === true){
-                    var titlesize = titleget.offsetHeight - 200;
-                    var whereIbe = window.pageYOffset + stophere.getBoundingClientRect().top - chosen - titlesize;
-                    
-                    console.log(whereIbe);
-                    if(whereIbe > 504){
+	var observermaybe = new IntersectionObserver(function(entries) {
+	if (entries[0].isIntersecting === true){
+		var titlesize = titleget.offsetHeight - 200;
+		var whereIbe = window.pageYOffset + stophere.getBoundingClientRect().top - chosen - titlesize;
+
+		console.log(whereIbe);
+		if(whereIbe > 504){
 			pageget.style.translate = '0px ' + whereIbe + 'px';
-                        console.log("Get over here!");
-                    }
-                } else {
-			pageget.style.translate = '0';
-                }
-            }, { threshold: [0] });
-            observermaybe.observe(checkpoint);
-        })
+			console.log("Get over here!");
+		}
+	} else {
+		pageget.style.translate = '0';
+	}
+	}, { threshold: [0] });
+	observermaybe.observe(checkpoint);
+})
