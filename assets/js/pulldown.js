78cb9805c3de64a72c3a1937737226eff197229d
Kenneth Herndon <kengherndon2@gmail.com>
2025-11-23 08:15:40 -0600

    changes to pulldown
    trying to make it where it will change the amount it pulls down if it
    knows it's a mini

--- assets/js/pulldown.js
+++ assets/js/pulldown.js
@@ -1,13 +1,33 @@
+var howfardown = [2075, 1575]
+var chosen
 document.addEventListener('DOMContentLoaded', function() {
-            var checkpoint = document.querySelector('#catch-up');
-            var pageget = document.getElementById('nextpage');
-            var stophere = document.getElementById('catch-up');
-            var titleget = document.getElementById('Title');
+	var checkpoint = document.querySelector('#catch-up');
+	var pageget = document.getElementById('nextpage');
+	var stophere = document.getElementById('catch-up');
+	var titleget = document.getElementById('Title');
+	var body = document.querySelector('body');
     
+	switch (pageget){ /*cfw legacy page assurance*/
+		case null:
+			pageget = document.getElementById('nextone');
+			break;
+		default:
+			break;
+	}
+
+	switch (true) {
+		case body.classList.contains('mini'):
+			chosen = howfardown[1];
+			break;
+		default:
+			chosen = howfardown[0];
+			break;
+	}
+
             var observermaybe = new IntersectionObserver(function(entries) {
                 if (entries[0].isIntersecting === true){
                     var titlesize = titleget.offsetHeight - 200;
-                    var whereIbe = window.pageYOffset + stophere.getBoundingClientRect().top - 2075 - titlesize;
+                    var whereIbe = window.pageYOffset + stophere.getBoundingClientRect().top - chosen - titlesize;
                     
                     console.log(whereIbe);
                     if(whereIbe > 504){
