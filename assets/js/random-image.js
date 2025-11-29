ffcfb2e76c10cb787eaa2ae8737defb5bcf52f8b
Kenneth Herndon <kengherndon2@gmail.com>
2025-11-29 16:17:06 -0600

    added potential pfps and random picker
    it works but i wanna see if i can make it where each instance of a guest
    has a different pic

--- assets/js/random-image.js
+++ assets/js/random-image.js
@@ -1,15 +1,26 @@
 /*give an image (prolly cover image) a class of "randomimg" and set the relevent images*/
+/*bahaha you spelling relevant wrong- IDIOT!!!*/
 
 document.addEventListener('DOMContentLoaded', function() {
 	let pic_target = document.getElementsByClassName('randomimg'); 
+	let pfp_target = document.getElementsByClassName('guest')
+	let target = [pic_target, pfp_target]
 
 	function choose() {
-		var a = Math.floor(Math.random() * relevent.length);
-		var choice = path + relevent[a];
+		let a = Math.floor(Math.random() * relevent.length);
+		let choice = path + relevent[a];
 		pic_target[0].src = choice;
 
 	}
 
+	if (target[0].length > 0 || target[1].length > 0) {
+		switch (pfp_target[0]) {
+			case undefined:
+				break;
+			default:
+				superchoose();
+		}	
+	}
 	switch (pic_target[0]) {
 		case undefined:
 			break;
@@ -18,6 +29,19 @@ document.addEventListener('DOMContentLoaded', function() {
 			break;
 	}
 	
+	function superchoose() {
+		let choice
+		let d = Math.floor(Math.random() * relevent.length);
+		let e = Math.floor(Math.random() * relevent[d].length);
+		console.log(relevent[d][e])
+		choice = path + relevent[d][e];
+		for (let superchosen of pfp_target) {
+			superchosen.src = choice;
+		}
+		
+	}
 })
 
-
+function oops_pfp(image) {
+	image.src = "assets/metacontent/pfps/air8.webp"
+}
