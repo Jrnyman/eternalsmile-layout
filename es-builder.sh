a4d4390fd7b5589013b323003ac09f35521fed53
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-20 14:55:08 -0600

    space

--- es-builder.sh
+++ es-builder.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/bash
+
 echo "es-builder"
 
 set -euf -o pipefail
 
 render() {
 	title="web page"
-	echo "$(eval "cat<<EOF
+	echo "$(eval "cat <<EOF
 		$(<${1/.meta/.html})
 EOF")"
 }
