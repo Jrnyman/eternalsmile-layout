705ff287cbaaf333ab632e73cc71699b5f0ed2bc
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-20 18:47:33 -0600

    added config for 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+
+
+	eleventyConfig.addPassthroughCopy("assets/metacontent");
+	eleventyConfig.addPassthroughCopy("assets/pagetray/");
+
+	eleventyConfig.addPassthroughCopy("assets/extra/");
+	eleventyConfig.addPassthroughCopy("assets/js/");
+};
