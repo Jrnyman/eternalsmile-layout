9fa6e7d86a42005eb970c78de30b096a81c0894b
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-20 19:01:07 -0600

    removing .trash from _site

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,8 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy("assets/metacontent");
 	eleventyConfig.addPassthroughCopy("assets/pagetray/");
-
 	eleventyConfig.addPassthroughCopy("assets/extra/");
 	eleventyConfig.addPassthroughCopy("assets/js/");
+
+	eleventyConfig.ignores.add(".trash");
 };
