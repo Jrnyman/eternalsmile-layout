9dbcd77e4e200b190aa7fce4abacb2068b3ce5c2
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-21 19:42:23 -0600

    pages can be created from template in array
    next i want to get all files in /input/ to assets/pagetray or somewhere
    that makes sense. then I want to have all the needed information for
    each page change what's gotta be changed on the page (current page, next
    page, publish date, sticker, ect)

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,5 +9,28 @@ module.exports = async function (eleventyConfig) {
 	eleventyConfig.ignores.add(".trash");
 	
 
+	//eleventyConfig.addCollection("images", function (collectionApi) { 
+	//	return glob(['assets/pagetray/**./input/*.{jpeg,jpg,png,webp}"]);
+	//})
 };
+
+const fakepages = [
+	{url: "assets/pagetray/1.png", sticker: false, pub_date: 11/12/25},
+	{url: "assets/pagetray/2.png", sticker: false, pub_date: 11/12/25},
+	{url: "assets/pagetray/3.png", sticker: false, pub_date: 11/12/25},
+	{url: "assets/pagetray/4.png", sticker: false, pub_date: 11/12/25},
+	{url: "assets/pagetray/5.png", sticker: false, pub_date: 11/12/25}
+]
+
+const fs = require("fs");
+const path = require("path");
+const { glob } = require('node:fs/promises');
+
+(async () => {
+
+	let files
+	for await (const entry of glob("./input/*.{png,gif,webp,jpg,jpeg}"))
+		console.log(entry);
+		entry = files;
+})();
 
