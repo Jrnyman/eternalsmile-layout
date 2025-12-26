766e404a55baaafc73f3cdec86a6cbdda5197be1
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-26 13:46:36 -0600

    added cases for defaulter

--- _data/scripts.js
+++ _data/scripts.js
@@ -6,24 +6,23 @@ let b = fs.readFileSync('_data/pagedata.json');
 let comicData = JSON.parse(a);
 let pageData = JSON.parse(b);
 
-//if (comicData.pageDefault == 'bw') {
-//	console.log("its bw!");
-//	pageData.forEach((page) => {
-//		Object.assign(page.assumed, page.bw); 
-//	});
-//};
-
 let choice = comicData.pageDefault
+
 function defaulter(f) {
 	console.log('f is equal to', f);
 	pageData.forEach((page) => {
 		Object.assign(page.assumed, page[f]);
 	});
 }
+
 switch (comicData.pageDefault) {
-	case 'bw':
-		defaulter(comicData.pageDefault);
+	case 'bw': case 'sketch': case 'color': case 'thumb':
+		defaulter(choice);
 		console.log("switched");
+		break;
+	default:
+		defaulter(bw);
+		break;
 };
 
 console.log(comicData.pageDefault, pageData[4].assumed);
