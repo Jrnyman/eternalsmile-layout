eb7c2c5e6599c075fa4e9cb737e012cd638448ca
Kenneth Herndon <kengherndon2@gmail.com>
2025-12-26 13:40:15 -0600

    made function to change defaults

--- _data/scripts.js
+++ _data/scripts.js
@@ -6,12 +6,24 @@ let b = fs.readFileSync('_data/pagedata.json');
 let comicData = JSON.parse(a);
 let pageData = JSON.parse(b);
 
-if (comicData.pageDefault == 'bw') {
-	console.log("its bw!");
+//if (comicData.pageDefault == 'bw') {
+//	console.log("its bw!");
+//	pageData.forEach((page) => {
+//		Object.assign(page.assumed, page.bw); 
+//	});
+//};
+
+let choice = comicData.pageDefault
+function defaulter(f) {
+	console.log('f is equal to', f);
 	pageData.forEach((page) => {
-//		page.assumed = "I've been touched!"
-		Object.assign(page.assumed, page.bw); 
+		Object.assign(page.assumed, page[f]);
 	});
+}
+switch (comicData.pageDefault) {
+	case 'bw':
+		defaulter(comicData.pageDefault);
+		console.log("switched");
 };
 
 console.log(comicData.pageDefault, pageData[4].assumed);
