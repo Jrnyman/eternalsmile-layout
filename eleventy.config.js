e6acd0071e2021730dc86c553bfe65b146891312
Kenneth Herndon <kengherndon2@gmail.com>
2026-01-31 16:51:04 -0600

    removed pfp from showing up in _sites

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,13 +3,16 @@ module.exports = async function (eleventyConfig) {
 	//eleventyConfig.addGlobalData("permalink", "/{{ page.filePathStem }}.html");
 
 	eleventyConfig.addGlobalData("layout", "top-bottom.html");
-	eleventyConfig.addPassthroughCopy("assets/metacontent");
+	eleventyConfig.addPassthroughCopy("assets/metacontent/icons");
+	eleventyConfig.addPassthroughCopy("assets/metacontent/nav");
+	eleventyConfig.addPassthroughCopy("assets/metacontent/fonts");
 	eleventyConfig.addPassthroughCopy("assets/extra/");
 	eleventyConfig.addPassthroughCopy("assets/js/");
 	eleventyConfig.addPassthroughCopy("assets/pagetray/input/");
 
 	eleventyConfig.ignores.add(".trash");
 	eleventyConfig.ignores.add("readme.md");
+	eleventyConfig.ignores.add("assets/metacontent/pfps");
 
 	eleventyConfig.addFilter("r", function(value){
 		let a = String(value)
